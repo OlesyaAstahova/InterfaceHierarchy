0d23c300e35ac1c9dd02a3e32c14f506e2917df7
Олесуа и Юля <astahova.98@gmail.com>
2018-03-06 09:55:56 +0700

    Проектирование иерархий интерфейсов

---
+++ InterfaceHierarchy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+    public interface IAdvancedDraw : IDrawable
+    {
+        void DrawInBoundingBox(int top, int left, int bottom, int right);
+        void DrawUpsideDown();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Interface Hierarchy *****");
+            //Вызывть на уровне объекта 
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpsideDown();
+
+            //Получить IAdvancedDraw явным образом
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+
+            Console.ReadLine();
+        }
+    }
+}
